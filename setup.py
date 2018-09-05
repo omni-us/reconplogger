d65b846db5258c32ee6c1d1337d67a5ee81d4286
nischalsi <nischal@searchink.com>
2018-09-05 16:21:58 +0200

    Fixing version initialisation

--- setup.py
+++ setup.py
@@ -6,22 +6,21 @@ from plogger import __version__
 NAME = 'plogger'
 VERSION = __version__
 DESCRIPTION = 'omni:us python logging package'
-LONG_DESCRIPTION = '\n\n'.join([
-    open('README.md').read()])
+#LONG_DESCRIPTION = '\n\n'.join([ open('README.md').read()])
 AUTHOR = 'Nischal Padmanabha'
 EMAIL = 'nischal@omnius.com'
 
 REQUIRED = ['logmatic-python==0.1.7', 'python-json-logger==0.1.9']
+print("Building package %s version %s"%(NAME, VERSION))
 
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description='',
     author=AUTHOR,
     author_email=EMAIL,
     packages=find_packages(exclude=('tests',)),
     install_requires=REQUIRED,
-    include_package_data=True,
     license='Omnius Copyright'
 )
