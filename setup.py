8612f00d967fda8af442dc176a4d949fc049b80e
nischalsi <nischal@searchink.com>
2019-10-22 14:21:32 +0200

    Supporting reading of version

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, Command
+import os
+import re
 
 NAME = next(filter(lambda x: x.startswith('name = '), open('setup.cfg').readlines())).strip().split()[-1]
 
-setup(version=__import__(NAME+'.__init__').__version__)
+version_regex = r'__version__ = ["\']([^"\']*)["\']'
+with open(os.path.join(NAME, '__version__.py'), 'r') as version_file:
+    version_lines = version_file.read()
+    match = re.search(version_regex, version_lines)
+
+
+setup(version=match.group(1))
