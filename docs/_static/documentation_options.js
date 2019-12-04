cef06bbafbdc5c5594548c44fdd40caf2ec062f3
Mauricio Villegas <mauricio_ville@yahoo.com>
2019-12-04 15:12:35 +0100

    Bump version: 2.1.0 → 2.1.1

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master/v2.1.0+2commits/2019.12.04',
+    VERSION: '2.1.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
