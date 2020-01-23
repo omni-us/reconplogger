579663f615caad3fc387f390f612254ceaf0dba8
Mauricio Villegas <mauricio_ville@yahoo.com>
2020-01-23 15:16:29 +0100

    - Added nojekyll file for github page.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master/v2.3.1+11commits/2020.01.23',
+    VERSION: 'master/v2.3.1+12commits/2020.01.23',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
