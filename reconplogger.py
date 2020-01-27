d7aeee37a7d94a4b9fa4f14955d1ecd7b05912dc
nischalhp <nischal.hp@gmail.com>
2020-01-27 16:38:09 +0100

    Bump version: 2.3.2 → 3.0.0

--- reconplogger.py
+++ reconplogger.py
@@ -5,7 +5,7 @@ import logging
 import logging.config
 
 
-__version__ = '2.3.2'
+__version__ = '3.0.0'
 
 
 reconplogger_format = '%(asctime)s\t%(levelname)s -- %(filename)s:%(lineno)s -- %(message)s'
