4fae8db38abe245412a4fbaf524948406bbb4f8d
Mauricio Villegas <mauricio_ville@yahoo.com>
2020-01-28 10:08:52 +0100

    - Added code coverage to circleci config.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master/v3.0.0+2commits/2020.01.28',
+    VERSION: 'master/v3.0.0+3commits/2020.01.28',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
